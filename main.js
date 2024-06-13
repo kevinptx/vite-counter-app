1b4c535bcf561e0efad8eb5e2c55a1d10a4d09ad
Kevin Peery <kevin.peery@gmail.com>
2024-06-12 20:28:01 -0500

    vite-counter-app

---
+++ main.js
@@ -0,0 +1,14 @@
+import setupCounter from "./src/counter.js";
+import "./styles.css";
+
+console.log("Hello from main.js");
+
+document.querySelector("#app").innerHTML = `
+  <h1>Counter App</h1>
+
+  <p>Click the button to increase the counter.</p>
+
+  <div id="counter"></div>
+`;
+
+setupCounter(document.querySelector("#counter"));
